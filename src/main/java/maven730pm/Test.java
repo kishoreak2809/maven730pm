23daf45ac293c51b1e7ec9fd0cb45456e368c978
kishore <kishorejii11@gmail.com>
2022-06-08 18:41:29 +0530

    project strucutre

---
+++ src/main/java/maven730pm/Test.java
@@ -0,0 +1,5 @@
+package maven730pm;
+
+public @interface Test {
+
+}
---
+++ src/main/java/maven730pm/baseclass.java
@@ -0,0 +1,25 @@
+package maven730pm;
+
+public class baseclass {
+
+	
+	@Test
+	private void test() {
+	 System.out.println("hello");
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
